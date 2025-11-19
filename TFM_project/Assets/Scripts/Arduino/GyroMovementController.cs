46e47d04c99a0a30fddd87c5691c22d7fa93aa55
Samantha Esparza-Baker <esparzs1@my.erau.edu>
2025-11-18 23:56:03 -0700

    new scripts
    new movement to trigger rather matching rotation with gyro rotation

---
+++ TFM_project/Assets/Scripts/Arduino/GyroMovementController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class GyroMovementController : MonoBehaviour
+{
+    [Header("Limb References")]
+    public GyroLimb leftLimb;
+    public GyroLimb rightLimb;
+
+    [Header("Animation")]
+    public Animator anim; //Anims allow root motion to apply physics
+
+    [Header("Limb Settings")]
+    public string pairType = "arm";  
+    public float heightDiffThreshold = 0.15f;
+
+    void Update()
+    {
+        if (!leftLimb || !rightLimb || !anim) return;
+
+        float L = leftLimb.heightValue;
+        float R = rightLimb.heightValue;
+
+        bool leftUp = leftLimb.isRaised;
+        bool rightUp = rightLimb.isRaised;
+
+        // IF both limbs raised trigger jump or climb anim 
+        if (leftUp && rightUp)
+        {
+            if (pairType == "arm")
+                anim.SetTrigger("Climb");
+            else
+                anim.SetTrigger("Jump");
+
+            return;
+        }
+
+        // IF one limb higher than the other THEN trigger half-step or one-arm raise animations
+        if (L > R + heightDiffThreshold)
+        {
+            if (pairType == "arm")
+                anim.SetTrigger("LeftArmRaise");
+            else
+                anim.SetTrigger("LeftHalfStep");
+
+            return;
+        }
+
+        if (R > L + heightDiffThreshold)
+        {
+            if (pairType == "arm")
+                anim.SetTrigger("RightArmRaise");
+            else
+                anim.SetTrigger("RightHalfStep");
+
+            return;
+        }
+    }
+}
